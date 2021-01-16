9ad5ca840ccb871ad68680a0316e26708e5cf0c2
aniskchaou <kchaouanis26@gmail.com>
2021-01-16 09:30:32 +0100

    filter+entries

--- src/app/modules/general/dashboard/dashboard.component.ts
+++ src/app/modules/general/dashboard/dashboard.component.ts
@@ -12,11 +12,13 @@ export class DashboardComponent implements OnInit {
   ngOnInit(): void {
    
     let data = [20000, 14000, 12000, 15000, 18000, 19000, 22000];
+    let data2 = [90000, 78000, 65000, 44000, 98000, 32000, 43000];
+
     let labels =  ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
     var ctx1 = document.getElementsByClassName("revenue-chart");
     var ctx2 = document.getElementsByClassName("expense-chart");
-    this.renderChart(data, labels,ctx1,'rgb(255, 99, 132)');
-    this.renderChart(data, labels,ctx2,'rgb(128, 99, 132)');
+    this.renderChart(data, labels,ctx1,'rgba(245, 255, 61, 1)');
+    this.renderChart(data2, labels,ctx2,'rgba(5, 255, 30, 1)');
   }
 
 
